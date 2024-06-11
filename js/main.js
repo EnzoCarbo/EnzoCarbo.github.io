87d3ea0d371c5409f7c6a82f0ab3c692c13523e6
Enzo CARBONARA <enzo.carbonara@ynov.com>
2024-06-11 18:44:14 +0200

    fix bug scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    const offset = window.innerHeight / 2 - section.offsetHeight / 2;
+    const topPos = section.getBoundingClientRect().top + window.scrollY - offset;
+    window.scrollTo({
+        top: topPos,
+        behavior: 'smooth'
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 'auto',
